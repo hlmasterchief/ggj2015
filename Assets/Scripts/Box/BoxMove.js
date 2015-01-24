06dde1cd59e2165a30e1a1f026eca2a7616f103c
Dr.Yami <cuongnt.hn@gmail.com>
2015-01-24 22:33:31 +0200

    Box animation

--- Assets/Scripts/Box/BoxMove.js
+++ Assets/Scripts/Box/BoxMove.js
@@ -4,6 +4,7 @@ public var movedir : float = 1;
 public var speed : float = 2;
 public var top : float = 350;
 public var bot : float = 100;
+public var item : GameObject;
 
 function Start () {
 	movedir = Random.Range(0,2)*2-1;
