543f0615876e0be00db18f3651418d0e0bc82cdb
An Pham <hoakusa@gmail.com>
2015-01-24 17:54:07 +0200

    frog jump

---
+++ Assets/Scripts/Enemy/EnemyMove.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+public var speedx : float = 2;
+public var speedy : float = 2;
+public var g: float = 0.2;
+public var onair : boolean = true;
+public var character : GameObject;
+
+function Update () {
+	movement();	
+}
+
+function jump(){
+		speedy = 1;
+		onair = true;
+}
+
+function movement(){
+	
+	if (character.transform.position.x < transform.position.x) {
+		transform.position.x -= speedx;
+	} else {
+		transform.position.x += speedx;
+	}
+
+	if (onair == true){
+		speedy += -g*Time.deltaTime/0.2;
+		transform.position.y += speedy;
+	} else {
+		jump();		
+	}
+}
+
+function OnTriggerEnter2D (col: Collider2D) {
+	if (col.tag =="Ground"){
+		onair = false;
+		print("yo");
+	}
+}
