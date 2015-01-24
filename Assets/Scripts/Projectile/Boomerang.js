e57c517923072db58c055ae13786faf487441402
Phuc <t3ngph00@students.oamk.fi>
2015-01-24 21:02:48 +0200

    boomerang

--- Assets/Scripts/Projectile/Boomerang.js
+++ Assets/Scripts/Projectile/Boomerang.js
@@ -5,6 +5,11 @@ public var vix : float = 5;
 public var viy : float = 5;
 public var ax: float = 0.1;
 public var ay: float = 1;
+var tmpx : float; 
+var tmpy : float;
+var posx : float;
+var posy :float;
+var angle: float = 30;
 var a: float;
 var t: float;
 var y : float;
@@ -14,8 +19,23 @@ var flag: boolean = true;
 function Start() {
 	vx = vix;
 	vy = viy;	
-	x = transform.position.x;
-	y = transform.position.y;
+	tmpx = transform.position.x;
+	tmpy = transform.position.y;
+	posx = tmpx;
+	posy = tmpy;
+	a = angle*3.1416/180;
+	
+}
+
+function rotate(u: float, v: float, k:int){
+	//quay x
+	if (k ==1) {
+		return u*Mathf.Cos(a) - v*Mathf.Sin(a)-(posx*Mathf.Cos(a) - posy*Mathf.Sin(a))+posx;
+	}
+	//quay y
+	else {
+		return v*Mathf.Cos(a) + u*Mathf.Sin(a)-(posy*Mathf.Cos(a) + posx*Mathf.Sin(a))+posy;
+	}
 }
 
 function Update () {
@@ -24,23 +44,20 @@ function Update () {
 	} else {
 		movementdown();
 	}
+	transform.position.x = rotate(tmpx,tmpy,1);
+	transform.position.y = rotate(tmpx,tmpy,2);
 	
 }
 
-function addforce(){
-	var acc : float = 5;
-	
-}
 
 function movementup(){
-	transform.position.x +=vx;
-	transform.position.y +=vy;	 		
+	tmpx +=vx;
+	tmpy +=vy;	 		
 	vy -=ay;
 	
-	if ((Mathf.Abs(transform.position.y - y) <0.001)){
+	if ((Mathf.Abs(tmpy - posy) <0.001)){
 		vy = - viy;
 		ay = -1;
-		print("quay");
 		vx=-vx;
 		flag = false;
 	}	
@@ -48,16 +65,14 @@ function movementup(){
 
 
 function movementdown(){
-	transform.position.x +=vx;
-	transform.position.y +=vy;	 		
+	tmpx +=vx;
+	tmpy +=vy;	 		
 	vy -=ay;
 	
-	if ((Mathf.Abs(transform.position.y - y) <0.01)){
+	if ((Mathf.Abs(tmpy - posy) <0.01)){
 		vy =  viy;
 		vx=-vx;
-		ay = 1;
-		print("quay1");
-		
+		ay = 1;		
 		flag = true;
 	}	
 }
