fd5e0c2dc11a8387f94739d0a2a16f57f18621fb
Dr.Yami <cuongnt.hn@gmail.com>
2015-01-24 16:38:00 +0200

    movement animation player

--- Assets/Scripts/Player/Move.js
+++ Assets/Scripts/Player/Move.js
@@ -1,18 +1,52 @@
 ﻿#pragma strict
 
-var moveLeft : KeyCode;
-var moveRight : KeyCode;
-var speed : float = 50;
+public var moveLeft : KeyCode;
+public var moveRight : KeyCode;
+public var speed : float = 5;
 
-function Update () {
+private var faceRight : boolean = true;
 
+function FixedUpdate () {
+	var animator: Animator = GetComponent(Animator);
+
+	// key down
+	// move when key down
 	if (Input.GetKey(moveLeft)){
-		transform.position.x -= speed;		
+		transform.localPosition.x -= speed;
+		animator.SetBool("move", true);
+		if (faceRight) {
+			FlipFace();
+		}
 	} else if (Input.GetKey(moveRight)){
-		transform.position.x += speed;
+		transform.localPosition.x += speed;
+		animator.SetBool("move", true);
+		if (!faceRight) {
+			FlipFace();
+		}
 	} 	
+	
+	// key up
+	// set animation when not move
+	if (Input.GetKeyUp(moveLeft)) {
+		if (!faceRight) {
+			animator.SetBool("move", false);
+		}
+	}
+	
+	if (Input.GetKeyUp(moveRight)) {
+		if (faceRight) {
+			animator.SetBool("move", false);
+		}
+	}
+}
+
+function FlipFace() {
+	var scaleX = transform.localScale.x;
+	faceRight = !faceRight;
+	scaleX = scaleX * -1;
+	transform.localScale.x = scaleX;
 }
 
 function OnTriggerEnter2D (other: Collider2D) {
-	print("yo");
+	
 }
