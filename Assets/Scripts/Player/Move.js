80257ece761c528698636d04008c972951865ea9
Phuc <t3ngph00@students.oamk.fi>
2015-01-24 17:00:44 +0200

    add jumping

--- Assets/Scripts/Player/Move.js
+++ Assets/Scripts/Player/Move.js
@@ -1,18 +1,41 @@
 ﻿#pragma strict
 
-var moveLeft : KeyCode;
-var moveRight : KeyCode;
-var speed : float = 50;
+public var moveLeft : KeyCode;
+public var moveRight : KeyCode;
+public var space: KeyCode;
+public var speedx : float = 1;
+public var speedy : float = 10;
+public var g: float = 0.2;
+public var onair : boolean = true;
 
 function Update () {
-
-	if (Input.GetKey(moveLeft)){
-		transform.position.x -= speed;		
-	} else if (Input.GetKey(moveRight)){
-		transform.position.x += speed;
-	} 	
+	movement();	
 }
 
-function OnTriggerEnter2D (other: Collider2D) {
-	print("yo");
+function jump(){
+	if (Input.GetKey(space)){
+		speedy = 7.5;
+		onair = true;
+	}
+}
+
+function movement(){
+	if (Input.GetKey(moveLeft)){
+		transform.position.x -= speedx;		
+	} else if (Input.GetKey(moveRight)){
+		transform.position.x += speedx;
+	} 	
+	if (onair == true){
+		speedy += -g*Time.deltaTime/0.2;
+		transform.position.y += speedy;		
+	} else {
+		jump();		
+	}
+}
+
+function OnTriggerEnter2D (col: Collider2D) {
+	if (col.tag =="Ground"){
+		onair = false;
+		print("yo");
+	}
 }
