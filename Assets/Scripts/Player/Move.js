c720f534098baba784a57ffa54a862aece5b8867
Phuc <t3ngph00@students.oamk.fi>
2015-01-24 16:01:02 +0200

    Movement for player

---
+++ Assets/Scripts/Player/Move.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+var speed : float = 50;
+
+function Update () {
+
+	if (Input.GetKey(moveLeft)){
+		transform.position.x -= speed;		
+	} else if (Input.GetKey(moveRight)){
+		transform.position.x += speed;
+	} 	
+}
+
+function OnTriggerEnter2D (other: Collider2D) {
+	print("yo");
+}
