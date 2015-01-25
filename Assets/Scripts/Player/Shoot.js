136274b3dde6417f7086235073a237a8d8312dc1
Long Hoang <hlmasterchief93@gmail.com>
2015-01-25 08:52:07 +0200

    limit shoot

--- Assets/Scripts/Player/Shoot.js
+++ Assets/Scripts/Player/Shoot.js
@@ -2,33 +2,41 @@
 
 var boomerang : GameObject;
 public var shoot : KeyCode;
+private var bmr : GameObject;
+private var bmrc : Boomerang;
 
 function Start () {
 
 }
 
 function Update () {
-	if (Input.GetKeyDown(shoot)){
+	if (bmr){
+		var base = GameObject.Find("Base");
+		if ((Mathf.Abs(bmrc.transform.position.x) > base.collider2D.bounds.max.x) || (bmrc.transform.position.y < -200) || (bmrc.transform.position.y > base.collider2D.bounds.max.y)) {	
+			 Destroy(bmr);
+		 }
+	} else {
+		if (Input.GetKeyDown(shoot)){
 
-		var mouse = Input.mousePosition;
-		mouse.z = 0;
-		mouse = Camera.main.ScreenToWorldPoint(mouse);
-		
-		var bmr : GameObject;
-		var bmrc : Boomerang;
-		bmr = Instantiate(boomerang, Vector3 (0, 0, 0), Quaternion.identity);
-		bmrc = bmr.GetComponent("Boomerang");
+			var mouse = Input.mousePosition;
+			mouse.z = 0;
+			mouse = Camera.main.ScreenToWorldPoint(mouse);
+			
 
-		bmrc.transform.position.x = transform.position.x;
-		bmrc.transform.position.y = transform.position.y;
-		
-		bmrc.posx = transform.position.x;
-		bmrc.posy = transform.position.y;
+			bmr = Instantiate(boomerang, Vector3 (0, 0, 0), Quaternion.identity);
+			bmrc = bmr.GetComponent("Boomerang");
 
-		bmrc.a = Mathf.Atan((mouse.y - bmrc.posy) / (mouse.x - bmrc.posx));
-		if ((mouse.x - bmrc.posx) < 0){
-			bmrc.a = Mathf.PI + bmrc.a;
+			bmrc.transform.position.x = transform.position.x;
+			bmrc.transform.position.y = transform.position.y;
+			
+			bmrc.posx = transform.position.x;
+			bmrc.posy = transform.position.y;
+
+			bmrc.a = Mathf.Atan((mouse.y - bmrc.posy) / (mouse.x - bmrc.posx));
+			if ((mouse.x - bmrc.posx) < 0){
+				bmrc.a = Mathf.PI + bmrc.a;
+			}
+			bmrc.start = true;
 		}
-		bmrc.start = true;
 	}
 }
